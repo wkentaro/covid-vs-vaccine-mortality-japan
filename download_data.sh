fdf6baab272e132e7496b3c62ed628c2401af490
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-07-11 00:53:27 +0900

    Use vaccination data on 2021-07-07

--- download_data.sh
+++ download_data.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# http://www.ipss.go.jp/projects/j/Choju/covid19/
+# @2021-07-05
 curl http://www.ipss.go.jp/projects/j/Choju/covid19/data/japan_deaths.xlsx -o data/japan_covid_death.xlsx
 
+# @2021-06
 curl https://www.stat.go.jp/data/jinsui/pdf/202106.pdf -o data/japan_population.pdf
 
+# @2021-07-07
 curl https://www.mhlw.go.jp/content/10601000/000802338.pdf -o data/japan_vaccine_death.pdf
 
-curl https://vrs-data.cio.go.jp/vaccination/opendata/latest/prefecture.ndjson -o data/japan_vaccination.ndjson.gz
+# https://cio.go.jp/c19vaccine_opendata
+# @2021-07-07
+curl https://vrs-data.cio.go.jp/vaccination/opendata/2021-07-07/prefecture.ndjson -o data/japan_vaccination.ndjson.gz
 rm -f data/japan_vaccination.ndjson
 gunzip data/japan_vaccination.ndjson.gz
